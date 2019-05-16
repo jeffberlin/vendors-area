0f550f1fb6dc58ab31633cfb56cc5499738a6b3a
jeffberlin <jeff@jbrally.com>
2019-05-16 11:43:50 -0400

    Formatting

--- js/main.js
+++ js/main.js
@@ -1,10 +1,10 @@
 // Redirect to login page if this page is accessed and the vendor is not logged in
 if (getCookieValue ("BMTMicro.Vendors.VendorID") == null) {
-    window.location = "https://vendors-new.bmtmicro.com/login.html";
+  window.location = "https://vendors-new.bmtmicro.com/login.html";
 }
 // Function for pulling name and company when available
 //function getVendorName () {
-    var n = getCookieValue ("BMTMicro.Vendors.Name");
-    var c = getCookieValue ("BMTMicro.Vendors.Company");
-    //return (((c == null) || (c == "")) ? n : (n + "</br>" + c ));
+  var n = getCookieValue ("BMTMicro.Vendors.Name");
+  var c = getCookieValue ("BMTMicro.Vendors.Company");
+  //return (((c == null) || (c == "")) ? n : (n + "</br>" + c ));
 //}
